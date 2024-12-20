ef20b9a1b36e344e2ba158b3e8f5864063ffe0ad
Ankitvmohan <ankitvmohan0401@gmail.com>
2024-12-21 00:10:17 +0530

    high score upgradation

--- scoreboard.py
+++ scoreboard.py
@@ -9,6 +9,9 @@ class score_board(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("/python_programs/Udemy_bootcamp/PROJECTS/SnakeGame/data.txt") as data:
+            self.highscore = int(data.read())
+            
         self.color("White")
         self.penup()
         self.goto(0,270)
@@ -17,15 +20,19 @@ class score_board(Turtle):
         self.update_score()
         
     def update_score(self):
-        self.write(f"Score: {self.score}",align = ALIGNMENT, font =FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.highscore}",align = ALIGNMENT, font =FONT)
+    
+    def reset(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+            with open("/../../../../data.txt", mode = 'w') as data:
+                data.write(f"{self.highscore}")
+        self.score = 0
+        self.update_score()
+        
         
     def increase_score(self):
         self.score += 1
-        self.clear()
-        self.update_score()
-        
-    def game_over(self):
-        self.goto(0,0)
-        self.write(f"GAME OVER",align = ALIGNMENT, font =FONT)
-        
+        self.update_score()       
         
